a2dcab0ce42961277bf91c9d9b6f42b169a7b022
gotthes <mustayev0234@mail.ru>
2025-11-19 20:21:30 +0500

    random generation (40 x 100)

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-#Minesweeper
+import random
+
+
+def create_minesweeper_field():
+    positions = list(range(100))
+    mine_positions = random.sample(positions, 40)
+
+    field = [[0 for _ in range(10)] for _ in range(10)] #поле 10х10
+
+    for pos in mine_positions:
+        row = pos // 10
+        col = pos % 10
+        field[row][col] = 1
+
+    return field
+
+def print_field(field):
+    for i, row in enumerate(field):
+        for cell in row:
+            if cell == 1:
+                print("X", end=" ")  #мина
+            else:
+                print(".", end=" ")  #пустая клетка
+        print()
+
+
+field = create_minesweeper_field()
+print_field(field)
